2b02e996014b4f85b2a31684d2142903d13a1924
Rawan-abdelfattah <rwanabdelfattah301@gmail.com>
2025-04-11 14:43:56 +0200

    change api

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-  baseURL: "http://45.93.139.193:6001/api",
+  baseURL: "https://doctor-bot.medicalonlineacademy.com/api",
   // baseURL: "http://localhost:5000/api/",
   headers: {
     "Content-Type": "application/json",
