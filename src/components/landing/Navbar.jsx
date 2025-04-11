94663e6325eb7680a7f888e9a60113c84188d755
Rawan-abdelfattah <rwanabdelfattah301@gmail.com>
2025-04-11 13:42:31 +0200

    edit on navbar

--- src/components/landing/Navbar.jsx
+++ src/components/landing/Navbar.jsx
@@ -1,25 +1,37 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
+  const location = useLocation();
+
+  const isActive = (path) =>
+    location.pathname === path
+      ? 'text-green-700 font-[30px] underline font-[800]'
+      : 'text-gray-600 hover:text-green-600';
+
   return (
-<nav className="sticky top-0 z-50  ">
-  <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
-    {/* Logo - Left */}
-    <div className="flex-shrink-0">
-      <img src="/logo.png" alt="Logo" className="w-[130px] h-[130px]" />
-    </div>
+    <nav className="sticky top-0 z-50  text-[22px] font-[500]">
+      <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
+        <div className="flex-shrink-0">
+          <img src="/logo.png" alt="Logo" className="w-[130px] h-[130px]" />
+        </div>
 
-    {/* Links - Center */}
-    <div className="absolute left-1/2 transform -translate-x-1/2 hidden md:flex space-x-6">
-      <Link to="/" className="text-green-700 font-medium hover:underline">Home</Link>
-      <Link to="/features" className="text-gray-600 hover:text-green-600">Features</Link>
-      <Link to="/how-it-works" className="text-gray-600 hover:text-green-600">How it works</Link>
-      <Link to="/support" className="text-gray-600 hover:text-green-600">Support</Link>
-    </div>
-  </div>
-</nav>
-
+        <div className="absolute left-1/2 transform -translate-x-1/2 hidden md:flex space-x-6">
+          <Link to="/" className={isActive('/')}>
+            Home
+          </Link>
+          <Link to="/features" className={isActive('/features')}>
+            Features
+          </Link>
+          <Link to="/how-it-works" className={isActive('/how-it-works')}>
+            How it works
+          </Link>
+          <Link to="/support" className={isActive('/support')}>
+            Support
+          </Link>
+        </div>
+      </div>
+    </nav>
   );
 };
 
--- src/views/landing/Home.jsx
+++ src/views/landing/Home.jsx
@@ -1,16 +1,38 @@
-import { Book } from 'components/landing/Home/Book'
-import Chatbot from 'components/landing/Home/Chatbot'
-import Hero from 'components/landing/Home/Hero'
-import SmartDashboard from 'components/landing/Home/SmartDashboard'
-import React from 'react'
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+import { Book } from 'components/landing/Home/Book';
+import Chatbot from 'components/landing/Home/Chatbot';
+import Hero from 'components/landing/Home/Hero';
+import SmartDashboard from 'components/landing/Home/SmartDashboard';
 
 export const Home = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: false,
+      offset: 300,
+    });
+  }, []);
+
   return (
     <div>
-      <Hero/>
-      <SmartDashboard/>
-      <Book/>
-      <Chatbot/>
+      <div>
+        <Hero />
+      </div>
+
+      <div data-aos="fade-left">
+        <SmartDashboard />
+      </div>
+
+      <div data-aos="fade-left">
+        <Book />
+      </div>
+
+      <div data-aos="zoom-in">
+        <Chatbot />
+      </div>
     </div>
-  )
-}
+  );
+};
