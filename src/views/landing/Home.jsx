8c5152088f6fc574b23c2f09e0de38454234517d
Rawan-abdelfattah <rwanabdelfattah301@gmail.com>
2025-04-11 18:31:16 +0200

    remove aos

--- src/views/landing/Home.jsx
+++ src/views/landing/Home.jsx
@@ -1,38 +1,17 @@
 import React, { useEffect } from 'react';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
 import { Book } from 'components/landing/Home/Book';
 import Chatbot from 'components/landing/Home/Chatbot';
 import Hero from 'components/landing/Home/Hero';
 import SmartDashboard from 'components/landing/Home/SmartDashboard';
 
 export const Home = () => {
-  useEffect(() => {
-    AOS.init({
-      duration: 1000,
-      once: false,
-      offset: 300,
-    });
-  }, []);
 
   return (
-    <div>
-      <div>
-        <Hero />
-      </div>
-
-      <div data-aos="fade-left">
-        <SmartDashboard />
-      </div>
-
-      <div data-aos="fade-left">
-        <Book />
-      </div>
-
-      <div data-aos="zoom-in">
-        <Chatbot />
-      </div>
+    <div> 
+        <Hero /> 
+        <SmartDashboard /> 
+        <Book /> 
+        <Chatbot /> 
     </div>
   );
 };
